eb1e021551a26af436c7f54d1fa6555e1ee8f837
Jeff Betty <jbetty@uoguelph.ca>
2026-03-30 12:20:18 -0400

    added an init file

---
+++ init-app.sh
@@ -0,0 +1,6 @@
+# Then sync your environment
+cd ./backend
+uv init --python 3.13
+uv sync
+cd ../frontend
+npm install
--- run-app.sh
+++ run-app.sh
@@ -2,6 +2,5 @@
 cd backend
 uv run uvicorn app:app --reload --port 8080 &
 cd ../frontend
-npm install
 npm run dev &
 wait
